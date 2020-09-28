d66967204fc90cd048565bbc980ccd2fcdcf2c09
nugo <nugokvachadze@gmail.com>
2020-09-28 16:49:02 +0400

    Add comment list on collapse.

--- src/modules/topics/Topics.vue
+++ src/modules/topics/Topics.vue
@@ -12,11 +12,43 @@
               :img-src="topic.img"
               :header="topic.title" class="text-center"
       >
-        <b-card-body>
-          <b-card-text>
-            {{topic.description}}
-          </b-card-text>
+        <b-card-body class="pt-0 pb-4">
+          <div class="actions">
+            <b-button v-b-toggle.collapse-comment class="mr-2 ml-2"
+                      variant="outline-primary"
+                      size="sm">
+              <i class="far fa-comments"></i>
+              <span> - {{topic.title.length}} </span>
+            </b-button>
+            <b-button class="mr-2 ml-2" variant="outline-info" size="sm">View</b-button>
+          </div>
         </b-card-body>
+
+        <b-collapse id="collapse-comment" class="mt-3">
+          <b-card-footer>
+            <b-media>
+              <template v-slot:aside>
+                <b-img blank blank-color="#ccc" width="64" src="" alt="placeholder"></b-img>
+              </template>
+
+              <h5 class="mt-0">John Doe</h5>
+              <p>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+                Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+              </p>
+            </b-media>
+            <div class="comments-action">
+              <div class="row">
+                <div class="col-md-6">
+                  <b-form-input placeholder="Comment"></b-form-input>
+                </div>
+                <div class="col-md-6">
+                  <b-button size="sm" variant="outline-primary"> Comment</b-button>
+                </div>
+              </div>
+            </div>
+          </b-card-footer>
+        </b-collapse>
       </b-card>
     </b-card-group>
     <topics-form-modal/>
@@ -59,6 +91,14 @@
     width: 59%;
     margin-left: 25%;
     margin-top: 5%;
+  }
+
+  .actions {
+    float: right;
+  }
+
+  .comments-action {
+    float: right;
   }
 
 </style>
