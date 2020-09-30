0c6fe1056f1ab6e136dc8a1b107c1605d4b74fec
nugo <nugokvachadze@gmail.com>
2020-09-30 11:39:28 +0400

    Add ability to open comments modal from `TopicsView`.

--- src/modules/topics/views/TopicsView.vue
+++ src/modules/topics/views/TopicsView.vue
@@ -3,40 +3,56 @@
     <div class="go-back mb-3">
       <b-button @click="goBack" variant="outline-info" size="sm"><i class="fas fa-arrow-left"></i> Back</b-button>
     </div>
-    <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
-      <template v-slot:header>
-        {{topic.title}}
-      </template>
 
-      <template v-slot:lead>
-        <img class="mt-2 mb-5" width="100%" height="300px" :src="topic.img" alt="">
+    <b-card bg-variant="dark" text-variant="white" :title="topic.title">
+      <b-card-img class="mb-3" height="350px" :src="topic.img"/>
+      <b-card-text>
         {{topic.description}}
-      </template>
-
-      <hr class="my-4">
-
-      <p>
-        created at - <span> {{topic.created_at}} </span>
-      </p>
-    </b-jumbotron>
+      </b-card-text>
+      <b-card-footer>
+        {{topic.created_at}}
+        <b-button class="mr-2 ml-2 float-right"
+                  @click="showCommentsModal(topic.id)"
+                  variant="outline-primary"
+                  size="sm">
+          <i class="far fa-comments"></i>
+          <span> - {{topic.commentCount}} </span>
+        </b-button>
+      </b-card-footer>
+    </b-card>
+    <topics-comments-modal/>
   </div>
 </template>
 
 <script>
+  import {createNamespacedHelpers} from 'vuex'
+  import TopicsCommentsModal from "@/modules/topics/TopicsCommentsModal";
+
+  const {mapState, mapActions} = createNamespacedHelpers('topics');
+
+
   export default {
     name: "TopicsView",
+    components: {TopicsCommentsModal},
     data() {
       return {
         topic: {}
       }
     },
+    computed: {
+      ...mapState({
+        topics: state => state.topics
+      })
+    },
     methods: {
+      ...mapActions(['getTopics', 'showModal', 'showCommentsModal']),
       goBack() {
         this.$router.go(-1);
       }
     },
     beforeMount() {
       this.topic = this.$route.params.topic;
+      this.getTopics();
     }
   }
 </script>
