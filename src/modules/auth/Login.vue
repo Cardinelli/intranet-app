7b1372ad133e1854f2fc35960a7b28c57c73388a
nugo <nugokvachadze@gmail.com>
2020-10-02 11:57:16 +0400

    Improve `Login` component.

--- src/modules/auth/Login.vue
+++ src/modules/auth/Login.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="authentication-modal">
     <b-card bg-variant="light" class="mt-5 p-3">
-      <b-form-group
-        label-cols-lg="0"
-        label="Login"
-        label-align="center"
-        label-class="font-weight-bold pt-0"
-        class="m-1"
-      >
+      <form v-on:submit.prevent="actionLogin">
         <b-form-group
-          label-cols-sm="4"
-          label="Username"
-          label-align-sm="right"
-          label-for="username-email"
+          label-cols-lg="0"
+          label="Login"
+          label-align="center"
+          label-class="font-weight-bold pt-0"
+          class="m-1"
         >
-          <b-form-input v-model="loginForm.email" id="username-email"></b-form-input>
+          <div class="form-inputs">
+            <b-form-input autocomplete="email" class="mb-2" placeholder="Email" v-model="loginForm.email"
+                          id="username-email"></b-form-input>
+            <b-form-input autocomplete="password" class="mt-2 mb-2" placeholder="Password" type="password"
+                          v-model="loginForm.password"
+                          id="password"></b-form-input>
+          </div>
         </b-form-group>
-
-        <b-form-group
-          label-cols-sm="4"
-          label="Password"
-          label-align-sm="right"
-          label-for="password"
-        >
-          <b-form-input type="password" v-model="loginForm.password" id="password"></b-form-input>
-        </b-form-group>
-      </b-form-group>
-      <b-button @click="actionLogin()" variant="outline-light" size="sm">Login</b-button>
+        <div class="text-center">
+          <b-button type="submit" variant="outline-light"
+                    size="sm"
+          >Login
+            <b-spinner small v-if="loading" label="Spinning"></b-spinner>
+          </b-button>
+        </div>
+      </form>
     </b-card>
   </div>
 </template>
@@ -41,7 +39,7 @@
     name: "Login",
     data() {
       return {
-        loader: false,
+        loading: false,
         loginForm: {
           email: '',
           password: '',
@@ -51,12 +49,14 @@
     methods: {
       ...mapActions(['saveLoggedInUser']),
       actionLogin() {
+        this.loading = true;
         if (!this.loginForm.email || !this.loginForm.password) {
           console.log('error');
         }
         firebase.auth().signInWithEmailAndPassword(this.loginForm.email, this.loginForm.password)
           .then(
             user => {
+              this.loading = false;
               localStorage.setItem('user', user.user.uid)
               this.saveLoggedInUser(user);
               this.$router.push('/');
